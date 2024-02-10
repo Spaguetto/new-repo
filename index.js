49d98f7f892707d1bfd60d525fe7620b0525f37a
Miguel <mifergpt@gmail.com>
2024-02-10 18:33:40 -0500

    Cambios en el index.js_ miguel_belalcazar

---
+++ index.js
@@ -0,0 +1,22 @@
+//En este archivo todos vamos a escribir un console.log!
+
+
+console.log('Saludos del profe!');
+
+risthian-quin
+// cristhian quna 
+
+console.log("Estudiante de desarrollo de software");
+
+
+// mi nombre
+
+console.log('Daniel Stiven Diaz');
+
+> master
+
+
+//Miguel Fernando Belalcazar//
+console.log("Estudiante en Desarrollo WEB");
+
+
