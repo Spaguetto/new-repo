1e3f549324c6b618f837d2fb7ca2450cc7257018
Spaguetto <48161646+Spaguetto@users.noreply.github.com>
2024-02-12 19:43:33 -0500

    hola!

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
 //En este archivo todos vamos a escribir un console.log!
 
+console.log("Saludos del profe!");
 
-console.log('Saludos del profe!');
-
-risthian-quin
-// cristhian quna 
+risthian - quin;
+// cristhian quna
 
 console.log("Estudiante de desarrollo de software");
 
-
 // mi nombre
 
-console.log('Daniel Stiven Diaz');
+console.log("Daniel Stiven Diaz");
 
-<<<<<<< HEAD
+console.log("Mateo Alejandro Salgado salgado");
 
-console.log('Mateo Alejandro Salgado salgado');
+console.log("Carlos Loaiza");
 
-
-console.log('Carlos Loaiza');
-
-
-
-=======
-
+alert("Hola a todos!, al profe le gusta el queso");
